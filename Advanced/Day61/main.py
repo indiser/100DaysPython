f9383e36fcfeb346e149f41244ca401706358c5c
indiser <138307236+indiser@users.noreply.github.com>
2025-12-27 21:08:58 +0530

    Add files via upload

---
+++ Advanced/Day61/main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,validators
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap5
+
+class MyForm(FlaskForm):
+    email = StringField(label="Email",validators=[DataRequired(),validators.email()])
+    password=PasswordField(label="Password",validators=[DataRequired(),validators.length(min=8)])
+    submit=SubmitField(label="Log In")
+
+
+app = Flask(__name__)
+app.secret_key="jsqjdqdvqdhhv"
+
+bootstrap=Bootstrap5(app)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    login_form=MyForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data=="admin@gmail.com" and login_form.password.data=="12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template("login.html",form=login_form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
